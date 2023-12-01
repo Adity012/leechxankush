82a3429ac831f2f330b74d189a6f0f71f6dbf7bf
Adity012 <149262704+Adity012@users.noreply.github.com>
2023-12-01 21:39:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ import os
 
 API_ID = 26368249
 API_HASH = "ac2db262d5eb7ce0efc0162404c8d172"
-BOT_TOKEN = "6713545950:AAG5Y-f4UDaVXeLnB1b1PV_NRW_OwaIk6m4"
+BOT_TOKEN = "6393747028:AAE8s8N0Jfh_-KXehs6Rby166z34iXdRLJs"
 bot = Client(
     "bot",
     bot_token=BOT_TOKEN,
